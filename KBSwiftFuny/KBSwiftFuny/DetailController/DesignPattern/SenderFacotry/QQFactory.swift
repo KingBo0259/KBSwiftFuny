c562dbaad1f5753f2955aa91a8d6a6e9fadcd5df
jinlb <jlb@kuaihuoyun.com>
2016-07-21 16:17:49 +0800

    add  工厂方法

---
+++ KBSwiftFuny/KBSwiftFuny/DetailController/DesignPattern/SenderFacotry/QQFactory.swift
@@ -0,0 +1,9 @@
+//
+//  QQFactory.swift
+//  KBSwiftFuny
+//
+//  Created by jinlb on 16/7/21.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+import Foundation
---
+++ KBSwiftFuny/KBSwiftFuny/DetailController/DesignPattern/SimpleFactory/Sender.swift
@@ -0,0 +1,9 @@
+//
+//  Sender.swift
+//  KBSwiftFuny
+//
+//  Created by jinlb on 16/7/21.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+import Foundation
